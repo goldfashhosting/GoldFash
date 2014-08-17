ccdfab78187c8661cee23e806b9d2cc8c0806b71
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-08-17 16:02:40 -0400

    Create GoldFash.php

---
+++ GoldFash.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: GoldFash
+Plugin URL: https://goldfash.com:443/plugins
+Description: A Required Sync Plugin to Access any GoldFash App
+Version: 0.1
+Author: GoldFash Design
+Author URI:        https://goldfash.com:443/
+Contributors:      raceanf
+Domain Path:       /languages
+Text Domain:       GoldFash-Dash
+GitHub Plugin URI: https://github.com/goldfashhosting/GoldFash
+GitHub Branch:     master
+*/
+
+		require( 'go.php'  );
+	
+?>
